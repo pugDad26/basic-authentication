17570742e4db5f46cf81a7b1588b902b6669262f
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2021-03-30 19:02:33 -0500

    reformatted code

--- src/Public.js
+++ src/Public.js
@@ -4,9 +4,11 @@ import Container from './Container';
 const Public = () => {
   return (
     <Container>
-      <h1>Public route</h1>
+      <h1>
+          Public route
+      </h1>
     </Container>
-  )
+  );
 }
 
 export default Public;
