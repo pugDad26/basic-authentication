1a0e6e5f3d0bac4353e200db69b3fce4b83874e1
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2021-04-06 17:51:41 -0500

    copy pasted reformatted this component

---
+++ src/Protected.js
@@ -0,0 +1,35 @@
+import 
+    React
+    , { useEffect } from 'react';
+
+import { Auth } from 'aws-amplify';
+import Container from './Container';
+
+const Protected = ({history}) => {
+    
+  useEffect(
+        () => {
+            Auth.currentAuthenticatedUser()
+            .then(
+                () => {
+                    console.log("User was authenticated");
+                }
+            )
+            .catch(
+                () => {
+                    props.history.push('/profile');
+                }
+            );
+        }
+        , []
+    );
+    return (
+        <Container>
+            <h1>
+                Protected route
+            </h1>
+        </Container>
+    );
+}
+
+export default Protected;
